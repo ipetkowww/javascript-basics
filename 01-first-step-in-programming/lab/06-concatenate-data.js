ba8cdb13dabe8bd0988fbd43d0a3f75f0b9ef51b
Ivan Petkov <petkow.iv@gmail.com>
2026-07-10 08:01:55 +0300

    [First step in coding] Solve last 4 tasks from lab

---
+++ 01-first-step-in-programming/lab/06-concatenate-data.js
@@ -0,0 +1,5 @@
+function printPersonInfo(firstName, lastName, age, town) {
+    console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`);
+}
+
+printPersonInfo("Maria", "Ivanova", "30", "Sofia");
